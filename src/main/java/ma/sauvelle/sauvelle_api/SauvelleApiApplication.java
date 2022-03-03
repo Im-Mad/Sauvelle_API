aed2f8bdff498e21da5118c891329c9f72d1dc12
Imad.chaichaa@linux <imadchai2@gmail.com>
2022-03-03 11:11:24 +0100

    setup

---
+++ src/main/java/ma/sauvelle/sauvelle_api/SauvelleApiApplication.java
@@ -0,0 +1,13 @@
+package ma.sauvelle.sauvelle_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SauvelleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SauvelleApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/sauvelle/sauvelle_api/SauvelleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.sauvelle.sauvelle_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SauvelleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
